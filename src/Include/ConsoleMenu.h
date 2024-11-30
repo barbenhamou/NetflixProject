3974cda165de6d4613f98b5237d8c87e5fa900d2
barbenhamou <barbenhamou2006@gmail.com>
2024-11-30 16:14:58 +0200

    NP-13 Created the classes

---
+++ src/Include/ConsoleMenu.h
@@ -0,0 +1,11 @@
+#include "IMenu.h"
+
+// This class represents the terminal's menu
+class ConsoleMenu : public IMenu {
+    public:
+        // This function will input a command from th user and parse it 
+        std::vector<std::string> nextCommand() override;
+
+        // This function will output an error message
+        void displayError(std::string error) override;
+};
--- src/Include/IMenu.h
+++ src/Include/IMenu.h
@@ -10,6 +10,7 @@ class IMenu {
     public:
         // All menus will need to process commands from the user, by implemnting this function
         virtual std::vector<std::string> nextCommand() = 0;
+        
         // All menus will have their own error message
         virtual void displayError(std::string error) = 0;
 };
