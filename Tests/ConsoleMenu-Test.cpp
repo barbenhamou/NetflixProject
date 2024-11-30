c4eb5bc1b5e325006ee9fc8be93da5ebe1276158
barbenhamou <barbenhamou2006@gmail.com>
2024-11-30 16:34:28 +0200

    NP-29 Bugfix

--- Tests/ConsoleMenu-Test.cpp
+++ Tests/ConsoleMenu-Test.cpp
@@ -2,19 +2,21 @@
 #include <../src/Include/ConsoleMenu.h>
 #include <memory>
 
+// Genric parsing test
 TEST(ConsoleMenuTest, NextCommandGnericForm) {
     std::istringstream simulatedInput("command arg1 arg2"); // Redirecting the stdin for the test
     std::cin.rdbuf(simulatedInput.rdbuf());
 
-    // Create an instance of ConsoleMenu
     std::unique_ptr<ConsoleMenu> menu = std::make_unique<ConsoleMenu>();
 
-    // Call the function
     std::vector<std::string> result = menu->nextCommand();
 
-    // Expected result
     std::vector<std::string> expected = {"command", " arg1 arg2"};
 
-    // Check if the result matches the expected value
-    EXPECT_EQ(result, expected);
+    EXPECT_EQ(result, expected); // Checking the expected output
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
