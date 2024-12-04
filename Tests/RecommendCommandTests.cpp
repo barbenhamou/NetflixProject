4cc539e6a35be857b69aee96578d8babad3a96aa
barbenhamou <barbenhamou2006@gmail.com>
2024-12-04 16:21:21 +0200

    NP-12 finished recommend tests

--- Tests/RecommendCommandTests.cpp
+++ Tests/RecommendCommandTests.cpp
@@ -28,3 +28,46 @@ TEST(RecommendationTests, SortByRelevance) {
     delete movie4;
     delete movie5;
 }
+
+TEST(RecommendationTests, Recommend) {
+    std::vector<std::pair<int, std::vector<int>>> data = {
+        {1, {100, 101, 102, 103}},
+        {2, {101, 102, 104, 105, 106}},
+        {3, {100, 104, 105, 107, 108}},
+        {4, {101, 105, 106, 107, 109, 110}},
+        {5, {100, 102, 103, 105, 108, 111}},
+        {6, {100, 103, 104, 110, 111, 112, 113}},
+        {7, {102, 105, 106, 107, 108, 109, 110}},
+        {8, {101, 104, 105, 106, 109, 111, 114}},
+        {9, {100, 103, 105, 107, 112, 113, 115}},
+        {10, {100, 102, 105, 106, 107, 109, 110, 116}}
+    };
+
+    for (int i = 0; i < data.size(); i++) {
+        AddCommand::add(data[i].first, data[i].second);
+    }
+
+    // Test cases 1-2: user hasn't watched movie
+    // Test cases 3-4: user watched movie
+    // vector of tuples that are <userId, movieId, output>
+    std::vector<std::tuple<int, int, std::vector<int>>> testCases = {
+        {1, 104, {105, 106, 111, 110, 112, 113, 107, 108, 109, 114}},
+        {3, 115, {103, 112, 113, 115, 101, 102, 106, 109, 110, 111}},
+        {7, 105, {100, 101, 104, 111, 116, 103, 114, 112, 113, 115}},
+        {10, 116, {101, 103, 104, 108, 111, 112, 113, 114, 115}}
+    };
+
+    User* user;
+    Movie* movie;
+    std::vector<int> expected;
+    std::vector<int> recommendations;
+
+    for (int i = 0; i < testCases.size(); i++) {
+        user = allUsers[User::findUser(std::get<0>(testCases[i]))].get();
+        movie = allMovies[Movie::findMovie(std::get<1>(testCases[i]))].get();
+        
+        expected = std::get<2>(testCases[i]);
+        recommendations = RecommendCommand::printRecommendations(RecommendCommand::recommend(user, movie));
+        if (i == 0) EXPECT_EQ(recommendations, expected);
+    }
+}
