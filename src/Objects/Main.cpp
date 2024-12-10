dbd2b98f2db9e6ae7dff2cb900caf4d24750f545
barbenhamou <barbenhamou2006@gmail.com>
2024-12-10 14:51:55 +0200

    NP-55 Created files

--- src/Objects/Main.cpp
+++ src/Objects/Main.cpp
@@ -1,6 +1,13 @@
 #include "../Include/App.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        return 1;
+    }
+
+    int server_port = stoi(argv[1]);
+
+    
     // Get all info from the file into global variables
     AddCommand::initGlobals("data/user_data.txt");
 
