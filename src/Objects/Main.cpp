195f73a5e45763306d6caffc394e63a3a938d4a2
yonathan23306@gmail.com <yonathan23306@gmail.com>
2024-12-11 11:10:35 +0200

    NP-75-Add a "statusCodes" global dictionary

--- src/Objects/Main.cpp
+++ src/Objects/Main.cpp
@@ -9,19 +9,21 @@ int main() {
     IMenu* menu = new ConsoleMenu();
 
     ICommand* help = new HelpCommand();
-    ICommand* add = new AddCommand();
+    ICommand* Post = new PostCommand();
+    ICommand* Patch = new PatchCommand();
     ICommand* recommend = new RecommendCommand();
-
+    
     // Define the commands
     commands["help"] = help;
-    commands["add"] = add;
+    commands["Post"] = Post;
     commands["recommend"] = recommend;
-
+    commands["Patch"] =Patch ;
     App app(menu, commands);
     app.run();
 
     delete menu;
     delete help;
-    delete add;
+    delete Post;
+    delete Patch;
     delete recommend;
 }
