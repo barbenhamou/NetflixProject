38873867963fa698fa8e418d3530b8938aecc63f
barbenhamou <barbenhamou2006@gmail.com>
2024-12-16 00:47:40 +0200

    NP-54 updated protocol

--- src/Objects/Client.py
+++ src/Objects/Client.py
@@ -1,8 +1,7 @@
 import socket
 import sys
-import struct
 
-BYTES = 4096
+BYTES = 1024  # The maximum number of bytes to receive per chunk
 
 # The client object
 class Client:
@@ -13,47 +12,49 @@ class Client:
 
     def run(self):
         try:
+            # Connect to the server
             self.__sock.connect((self.__server_ip, self.__server_port))
 
             while True:
+                # Get input from the user
                 msg = input("")
                 if not msg:
                     continue
 
                 msg_bytes = msg.encode()
-                size = len(msg_bytes)
 
-                # Send the size of the message first
-                self.__sock.sendall(struct.pack("!I", size))
+                # Send the message bytes directly
                 self.__sock.sendall(msg_bytes)
 
-                # Receive the size of the response
-                size_data = self.__sock.recv(4)
-                if not size_data:
-                    break
-                size = struct.unpack("!I", size_data)[0]
-
-                # Receive the actual response
+                # Receive the response in chunks of 1024 bytes
                 data = b""
-                while len(data) < size:
-                    chunk = self.__sock.recv(min(BYTES, size - len(data)))
-                    if not chunk:
+                while True:
+                    # Receive a chunk of data
+                    chunk = self.__sock.recv(BYTES)
+                    if not chunk:  # If no more data, exit the loop
                         break
                     data += chunk
 
+                    # If received chunk is less than 1024 bytes, the message has been received
+                    if len(chunk) < BYTES:
+                        break
+
                 print(data.decode())
         except:
+            # Close the socket in case of an error
             self.__sock.close()
             return
 
 
 def main():
+    # Ensure the correct number of arguments are passed
     if len(sys.argv) != 3:
         return
-    
-    SERVER_IP = sys.argv[1]
-    SERVER_PORT = int(sys.argv[2])
 
+    SERVER_IP = sys.argv[1]  # The server's IP address
+    SERVER_PORT = int(sys.argv[2])  # The server's port
+
+    # Initialize and run the client
     client = Client(port=SERVER_PORT, server_ip=SERVER_IP)
     client.run()
 
--- src/Objects/SocketMenu.cpp
+++ src/Objects/SocketMenu.cpp
@@ -4,48 +4,51 @@ SocketMenu::SocketMenu(int clientSocket) : clientSocket(clientSocket) {}
 
 std::vector<std::string> SocketMenu::nextCommand() {
     try {
-        // Receive the size of the incoming data first
-        uint32_t size = 0;
-        int bytes_recved = recv(this->clientSocket, &size, sizeof(size), 0);
+        std::string input;
+        char buffer[1024];
+        ssize_t bytes_recved;
 
-        if (bytes_recved != sizeof(size) || size <= 0) {
-            close(clientSocket);  // Close the socket on error
-            return {};
+        // Read until a newline is encountered
+        while ((bytes_recved = recv(this->clientSocket, buffer, sizeof(buffer) - 1, 0)) > 0) {
+            // Null-terminate the received data, makes concatenation to input availabe
+            buffer[bytes_recved] = '\0';
+            input += buffer;
+
+            // Check if we received the newline delimiter - end of request
+            if (input.find('\n') != std::string::npos) {
+                break;
+            }
         }
 
-        size = ntohl(size);  // Convert size from network byte order to host byte order
-
-        // Buffer to receive the data
-        std::vector<char> buffer(size + 1, '\0'); // +1 for null terminator
-        bytes_recved = recv(this->clientSocket, buffer.data(), size, 0);
-
-        if (bytes_recved != size) {  // handle exception if not all bytes are received
-            close(clientSocket);
+        // If no data received or an error occurred
+        if (bytes_recved <= 0) {
+            close(this->clientSocket);
             return {};
         }
-
-        std::string data(buffer.data());  // Convert received data to a string
 
         // Parse the command
-        std::istringstream ss(data);
+        std::istringstream ss(input);
         std::string command;
         ss >> command;
 
         // Return the command and its arguments
-        return {command, data.substr(command.size())};
+        return {command, input.substr(command.size())};
     } catch (...) {
-        close(clientSocket);  // Ensure socket closure on error
+        close(this->clientSocket);  // Ensure socket closure on error
         return {};
     }
 }
 
 void SocketMenu::sendOutput(std::string output) {
     try {
-        // Send the size of the output first
-        uint32_t size = htonl(output.size());
-        send(this->clientSocket, &size, sizeof(size), 0);
+        // Send the output message
+        ssize_t bytes_sent = send(this->clientSocket, output.c_str(), output.size(), 0);
 
-        // Send the actual output
-        send(this->clientSocket, output.c_str(), output.size(), 0);
-    } catch (...) {}
+        // An error occurred
+        if (bytes_sent == -1) {
+            close(this->clientSocket);
+        }
+    } catch (...) {
+        close(this->clientSocket);
+    }
 }
