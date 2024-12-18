d73e10ea854cbedc716e9d7b545b092cac6af0d4
barbenhamou <barbenhamou2006@gmail.com>
2024-12-18 14:28:12 +0200

    NP-55 created the class of the Thread Cliet Manager

---
+++ src/Objects/ThreadClientManager.cpp
@@ -0,0 +1,21 @@
+#include "../Include/ThreadClientManager.h"
+
+void ThreadClientManager::addTask(int clientSocket) {
+    this->workers.emplace_back([clientSocket]() {
+        ClientHandler handler(clientSocket);
+        handler.interact();
+    });
+}
+
+void ThreadClientManager::shutdown() {
+    for (auto& worker : this->workers) {
+            if (worker.joinable()) {
+                worker.join();
+        }
+    }
+}
+
+ThreadClientManager::~ThreadClientManager() {
+    this->shutdown();
+}
+
