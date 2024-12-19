91aac63bc4af4bfab1051d745ea7bce8fec0e69d
barbenhamou <barbenhamou2006@gmail.com>
2024-12-19 12:56:04 +0200

    NP-56 Client handler added destructor

--- src/Include/ClientHandler.h
+++ src/Include/ClientHandler.h
@@ -17,4 +17,7 @@ class ClientHandler {
 
         // Initiate the interaction with the client, in thread
         void interact();
+
+        // Destructor / Destoyer
+        ~ClientHandler() {close(this->clientSocket);}
 };
