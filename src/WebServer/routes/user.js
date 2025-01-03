551efff6b54acfbdadae10b88ecd706e55ae4be9
yonathan23306@gmail.com <yonathan23306@gmail.com>
2025-01-03 17:12:29 +0200

    NP-101-NP-105-chnaged_monor_things

--- src/WebServer/routes/user.js
+++ src/WebServer/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
 
-router.post('/users', userController.createUser); // Create a new user
-router.post('/tokens', userController.authenticateUser); // Authenticate user
+router.post('/api/users', userController.createUser); // Create a new user
+router.post('/api/tokens', userController.authenticateUser); // Authenticate user
 
 module.exports = router;
