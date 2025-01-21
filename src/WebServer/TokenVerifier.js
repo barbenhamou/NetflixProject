cffd9cd423de8e84d75b49285eea4ea8e6eee6ac
barbenhamou <barbenhamou2006@gmail.com>
2025-01-21 14:27:09 +0200

    NP-143 created created JWT

--- src/WebServer/TokenVerifier.js
+++ src/WebServer/TokenVerifier.js
@@ -1,15 +1,33 @@
-const tokenValidation = (req, res, next) => {
-    if (!req.headers['authorization']) {
-        return res.status(401).json({ error: 'Token is missing' });
-    }
+const userService  = require('./services/user');
+const jwt = require('jsonwebtoken');
 
-    const token = req.headers['authorization'].split(' ')[1];
-    if (!token) {
-        return res.status(401).json({ error: 'Token is missing' });
-    }
+const key = "secret";
 
-    req.token = token;
-    next();
+const tokenValidation = (adminActionBool) => {
+    return (req, res, next) => {
+        if (!req.headers['authorization']) {
+            return res.status(401).json({ error: 'Token is missing' });
+        }
+
+        const token = req.headers['authorization'].split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ error: 'Token is missing' });
+        }
+
+        try {
+            const decodedToken = jwt.verify(token, key);
+            
+            if (adminActionBool && decodedToken.name !== 'admin') {
+                return res.status(401).json({ error: 'Access denied: Admins only' });
+            }
+
+            req.token = decodedToken;
+
+            next();
+        } catch (error) {
+            return res.status(401).json({ error: 'Token is invalid' });
+        }
+    };
 };
 
 module.exports = { tokenValidation };
