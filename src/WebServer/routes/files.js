d6ac330c2e2003be47d8877f68bf94d8b970bbd4
barbenhamou <barbenhamou2006@gmail.com>
2025-02-05 23:58:44 +0200

    NP-172 names

--- src/WebServer/routes/files.js
+++ src/WebServer/routes/files.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const fileController = require('../controllers/file');
+const fileController = require('../controllers/files');
 const tokenVerifier = require('../TokenVerifier');
 
 const uploadMulter = fileController.createMulterForMovie().fields([
