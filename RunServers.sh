5655e2340843eca9b30d6654c779cc9b85bf40b3
trckui11 <arbelfel@gmail.com>
2025-02-11 22:53:53 +0200

    Make RunServers.sh executable

---
+++ RunServers.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+find_available_port() {
+    local port=$1
+    while true; do
+        if netstat -tuln | grep -q ":$port"; then
+            port=$((port + 1))
+        else
+            echo "$port"
+            return
+        fi
+    done
+}
+
+read -p "Enter your IP address: " ip
+
+# Automatically choose ports
+port1=$(find_available_port 3000)
+port2=$(find_available_port $((port1 + 1)))
+port3=$(find_available_port $((port2 + 1)))
+
+# Set up the configs
+mkdir -p src/WebServer/config
+
+cat <<EOF > src/WebServer/config/.env.main
+CONNECTION_STRING="mongodb://mongo:27017/db"
+WEB_PORT=${port1}
+CPP_PORT=${port2}
+REACT_PORT=${port3}
+CPP_IP="${ip}"
+SECRET="GiveUs100"
+EOF
+
+cat <<EOF > src/web_app/src/config.js
+export const backendPort = ${port1}
+EOF
+
+cat <<EOF > .env
+WEB_PORT=${port1}
+CPP_PORT=${port2}
+REACT_PORT=${port3}
+EOF
+
+echo "Once the compilation ends, the website will run at: http://localhost:${port3}"
+
+# Run all the servers
+docker-compose up --build
